6dc3cff03fba74651fa2170abcd7faeb95505060
jgoldsmith27 <jacob.goldsmith@gmail.com>
2024-02-10 20:33:41 -0500

    a

--- myproject/videorecorder/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VideorecorderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'videorecorder'
--- myproject/videorecorder/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import record_video
-
-urlpatterns = [
-    path('record/', record_video, name='record_video'),
-]
